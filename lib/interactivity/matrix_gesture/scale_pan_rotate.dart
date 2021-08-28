b2b809a334084fe328c97f563d9f5326c947826c
musahinist <sahinmusa1@gmail.com>
2021-08-28 13:57:48 +0300

    scale_pan_rotate cpml

--- lib/interactivity/matrix_gesture/scale_pan_rotate.dart
+++ lib/interactivity/matrix_gesture/scale_pan_rotate.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'gesture_matrix.dart';
-
 class ScalePanRotate extends StatefulWidget {
   const ScalePanRotate({Key? key}) : super(key: key);
 
@@ -10,21 +8,47 @@ class ScalePanRotate extends StatefulWidget {
 }
 
 class _ScalePanRotateState extends State<ScalePanRotate> {
-  Matrix4 matrix = Matrix4.identity();
+  Offset _offset = Offset.zero;
+  Offset _initialFocalPoint = Offset.zero;
+  Offset _sessionOffset = Offset.zero;
+
+  double _scale = 1.0;
+  double _angle = 0.0;
+  double _initialScale = 1.0;
+  double _initialAngle = 0.0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: MatrixGestureDetector(
-        onMatrixUpdate: (m, tm, sm, rm) {
+    return Transform.translate(
+      offset: _offset + _sessionOffset,
+      child: GestureDetector(
+        onScaleStart: (details) {
+          _initialFocalPoint = details.focalPoint;
+          _initialScale = _scale;
+          _initialAngle = _angle;
+        },
+        onScaleUpdate: (details) {
           setState(() {
-            matrix = m;
+            _sessionOffset = details.focalPoint - _initialFocalPoint;
+            _scale = _initialScale * details.scale;
+            _angle = _initialAngle + details.rotation;
           });
         },
-        child: Transform(
-          transform: matrix,
-          child: Container(
-            child: Image.network(
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/2048px-User_icon_2.svg.png'),
+        onScaleEnd: (details) {
+          setState(() {
+            _offset += _sessionOffset;
+            _sessionOffset = Offset.zero;
+          });
+        },
+        child: Transform.scale(
+          scale: _scale,
+          child: Transform.rotate(
+            angle: _angle,
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: Image.network(
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/2048px-User_icon_2.svg.png"),
+            ),
           ),
         ),
       ),
